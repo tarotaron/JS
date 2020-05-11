5ce39e7d434b0514649f7fd81ea6cfd0c667b76a
tarotaron <suzu9095@gmail.com>
2020-05-11 14:21:20 +0900

    Java_practice

---
+++ Java_practice/src/Chapter.java
@@ -0,0 +1,13 @@
+
+public class Chapter {
+	public static void main(String[] args) {
+		String name;
+		name = "suzu";
+		System.out.println(name);
+		name = "nami";
+		System.out.println(name);
+		int number;
+		number = 8;
+		System.out.println(number);
+	}
+}
---
+++ Java_practice/src/practice.java
@@ -0,0 +1,8 @@
+
+public class practice {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello WEBCANP!");
+		System.out.println("Hello Java!");
+	}
+}
