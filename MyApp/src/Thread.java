909f55b1f963e8510ea3ff14fac5b885bc6d15f9
tarotaron <suzu9095@gmail.com>
2020-05-18 21:51:56 +0900

    Thread

--- MyApp/src/Thread.java
+++ MyApp/src/Thread.java
@@ -1,20 +1,40 @@
-class MyRunnable implements Runnable {
-	@Override
-	public void run() {
-		for (int i = 0; i < 500; i++) {
-			System.out.print('*');
-		}
-	}
-}
+// Thread
 
-public class Thread {
-	public static void main (String[] args) {
-		MyRunnable r = new MyRunnable();
-		Thread t = new Thread(r);
-		t.start();
-		for (int i = 0; i < 500; i++) {
-			System.out.print('.');
-		}
+// class MyRunnable implements Runnable { // 関数型インターフェース
+//   @Override
+//   public void run() {
+//     for (int i = 0; i < 500; i++) {
+//       System.out.print('*');
+//     }
+//   }
+// }
 
-	}
+public class MyApp {
+
+  public static void main(String[] args) {
+    // MyRunnable r = new MyRunnable();
+    // Thread t = new Thread(r);
+    // t.start();
+
+    // ラムダ式
+    // (引数) -> {処理}
+    // new Thread(new Runnable() {
+    //   @Override
+    //   public void run() {
+    //     for (int i = 0; i < 500; i++) {
+    //       System.out.print('*');
+    //     }
+    //   }
+    // }).start();
+    new Thread(() -> {
+      for (int i = 0; i < 500; i++) {
+        System.out.print('*');
+      }
+    }).start();
+
+    for (int i = 0; i < 500; i++) {
+      System.out.print('.');
+    }
+  }
+
 }
